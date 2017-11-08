59925d618e11801863ccde07c7aa13c412453b6b
Enrico Scherlies <e.scherlies@me.com>
2017-11-08 15:32:02 +0100

    added search and type

--- features/crawl.js
+++ features/crawl.js
@@ -1,6 +1,6 @@
 
 describe('Chimp Mocha', function () {
-  describe('Page title', function () {
+  describe('Page title @watch', function () {
     it('should be set by the Meteor method', function () {
       browser.url('https://www.starcar.de/specials/kostenlos-mieten/');
       expect(browser.getTitle()).to.equal('Kostenlos mieten');
@@ -10,22 +10,31 @@ describe('Chimp Mocha', function () {
   describe('Get Page Content', function () {
     it('should get a list of stations @watch', function () {
       let results = browser.execute(function () {
-        let e = document.getElementsByClassName('togglelist-truck')[1].getElementsByClassName('togglelist-content-item');
+        const search = 'berlin' // searching for offerings to this destination
+        const carType = [
+          'togglelist-all',
+          'togglelist-car',
+          'togglelist-truck'
+        ][2] //change number vor cartype
+        let e = document
+          .getElementsByClassName(carType)[1] // get second result (orderd by destination)
+          .getElementsByClassName('togglelist-content-item'); //get items
         let arr = [];
         for (let i = 0; i < e.length; i++) {
+          //get DOMs containting city names
           arr.push(e[i].getElementsByClassName('uppercase'));
         };
-        let results = arr.map(e => {
-          let from = e[0].innerText.replace(/\s/g, '');
-          let to = e[1].innerText.replace(/\s/g, '');
+        let results = []
+        arr.map(e => {
+          let from = e[0].innerText.replace(/\s/g, '').padEnd(25);
+          let to = e[1].innerText.replace(/\s/g, '').padEnd(25);
           let link = e[1].innerHTML.match(/.*href="+(.*)+".*/)[1];
-          return `\nfrom: ${from}, to: ${to}\nlink: ${link}`
-          // console.log('from: %s, to: %s \nlink: %s', from, to, link);
+          if (to.toLowerCase().search(search) > -1) return results.push(`\n${from}--> ${to}${link}`)
         });
         return results;
       })
 
-      results.value.map( e => console.log(e))
+      results.value.map(e => console.log(e))
 
     });
   });
